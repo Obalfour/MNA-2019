484263d9a33e68c6d8cad77047a4551deca06b84
Martina Scomazzon <mscomazzon@itba.edu.ar>
2019-10-03 14:24:44 -0300

    Some bug fixes

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
 from os import listdir
 from os.path import join, isdir
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import svm
 import imageio
 from sys import stdin
-from  sklearn.ensemble import GradientBoostingClassifier
 from utils import *
-import argparse
 from matrixtools import *
 import configparser
 
@@ -18,10 +15,10 @@ config.read("./configFile.ini")
 HORIZONTAL= config.getint("Size", "HORIZONTAL") #92
 VERTICAL= config.getint("Size", "VERTICAL") #112
 
-# [Figure]
-FIGURES_PER_PERSON = config.getint("Figure", "FIGURES_PER_PERSON") #10
-PEOPLE_NO = config.getint("Figure", "PEOPLE_NO") #40
-FIGURE_PATH = config.get("Figure", "FIGURE_PATH") #../att_faces/
+# [Image]
+IMAGES_PER_PERSON = config.getint("Image", "IMAGES_PER_PERSON") #10
+PEOPLE_NO = config.getint("Image", "PEOPLE_NO") #40
+IMAGE_PATH = config.get("Image", "IMAGE_PATH") #../att_faces/
 
 # [Training]
 TEST_NO = config.getint("Training", "TEST_NO") #4
@@ -35,10 +32,9 @@ QUERY = config.get("Test","QUERY")
 EIGEN_FACES = config.getint("Test", "EIGEN_FACES") #60
 
 clf = svm.LinearSVC()
-#clf = GradientBoostingClassifier()
 # TRAINING
 
-images_training, person_training, names_dictionary = openImages(FIGURE_PATH, PEOPLE_NO, TRAINING_NO, HORIZONTAL*VERTICAL)
+images_training, person_training, names = openImages(IMAGE_PATH, PEOPLE_NO, TRAINING_NO, HORIZONTAL*VERTICAL)
 if METHOD == 'KPCA':
     images_training *= 255.0
     images_training -= 127.5
@@ -79,17 +75,17 @@ clf.fit(proy_training, person_training.ravel())
 test_path = QUERY
 while(True):
     print("Input face path")
-    picture_path = stdin.readline().rstrip().split()[0]
+    image_path = stdin.readline().rstrip().split()[0]
     if METHOD == 'KPCA':
-        a = np.reshape((imageio.imread(test_path + picture_path + '.pgm') - 127.5) / 127.5, [1, VERTICAL*HORIZONTAL])
+        a = np.reshape((imageio.imread(test_path + image_path + '.pgm') - 127.5) / 127.5, [1, VERTICAL*HORIZONTAL])
         unoML = np.ones([1, (PEOPLE_NO * TRAINING_NO)]) / (PEOPLE_NO * TRAINING_NO)
         Ktest = (np.dot(a, images_training.T) / (PEOPLE_NO * TRAINING_NO) + 1) ** degree
         Ktest = Ktest - np.dot(unoML, K) - np.dot(Ktest, unoM) + np.dot(unoML, np.dot(K, unoM))
         imtstproypre = np.dot(Ktest, alpha)
         proy_test = imtstproypre[:, 0:EIGEN_FACES]
     else:
-        a = np.reshape(imageio.imread(test_path + picture_path + '.pgm') / 255.0, [1, VERTICAL*HORIZONTAL])
+        a = np.reshape(imageio.imread(test_path + image_path + '.pgm') / 255.0, [1, VERTICAL*HORIZONTAL])
         a -= meanimage
         proy_test = np.dot(a, B.T)
     prediction = clf.predict(proy_test)
-    print(names_dictionary[prediction[0]//1])
+    print(names[prediction[0]//1])
